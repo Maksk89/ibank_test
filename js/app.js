78afd9e983e2fa47d6812981650924d413825640
Maksim Kormiltsev <newarty28@gmail.com>
2021-08-06 19:24:15 +0300

    second version

--- js/app.js
+++ js/app.js
@@ -10,39 +10,44 @@ const showLoader = (parentEl) => {
 const loadAccount = (parentEl, data, err) => {
     if (err) {
         parentEl.innerHTML = `
-        <div>Произошла ошибка.</div>
+        <div class="info">Произошла ошибка.</div>
+        <button class="retry">Повторить запрос</button>
         `;
+        const retryEl = parentEl.querySelector('.retry');
+        retryEl.onclick = (evt) => {
+            showLoader(accountsEl);
+            loadAccountServer(evt);
+        };
+
         return;
     }
-    console.log(data);
     parentEl.innerHTML = `
     <div class = "name">${data.account.name}</div>
     <div class = "number">${data.account.number}</div>
     <div class="balance"><span class="amount">${(data.account.amount).toString().replace('.', ',')}</span> ₽</div>
-    
     `;
 };
 
 
+const loadAccountServer = () => {
+
+    fetch(`${API_URL}/hw16`)
+
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('err.server');
+            }
+            return response.json();
+        })
+        .then((data) => {
+                loadAccount(accountsEl, data);
+            }
+        )
+        .catch((e) => {
+            loadAccount(accountsEl, null, 'err.common');
+        });
+}
+
 const accountsEl = document.getElementById('accounts-and-cards');
 showLoader(accountsEl);
-
-
-fetch(`${API_URL}/hw15`)
-
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('err.server');
-        }
-        return response.json();
-
-    })
-    .then((data) => {
-            loadAccount(accountsEl, data);
-        }
-    )
-    .catch((e) => {
-        loadAccount(accountsEl, null, 'err.common');
-    });
-
-
+loadAccountServer();
